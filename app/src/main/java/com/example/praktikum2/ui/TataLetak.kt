5fe51a43a4ad0b56adecf0a8d38a61baa5832b38
mahasiswa229 <mafaqih1945@gmail.com>
2025-10-16 14:57:34 +0700

    bikin import tl

--- app/src/main/java/com/example/praktikum2/ui/TataLetak.kt
+++ app/src/main/java/com/example/praktikum2/ui/TataLetak.kt
@@ -1,2 +1,18 @@
-package com.example.praktikum2.ui
+package com.example.praktikum2 // PASTIKAN package ini sama dengan MainActivity.kt
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.praktikum2.R // Import R class (resources)
 
